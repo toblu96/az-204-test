f716f2ca30953db6fc02f9146ba0220c2c0a334e
Tobias Blum <tobiasblum02@gmail.com>
2024-12-17 11:44:58 +0100

    add mssql integration

--- server/api/hello.get.ts
+++ server/api/hello.get.ts
@@ -1,5 +1,13 @@
-export default defineEventHandler(() => {
+import sql from 'mssql'
+
+export default defineEventHandler(async (event) => {
+    const connection = await sql.connect(useRuntimeConfig(event).db)
+
+
+    const data = await sql.query('SELECT * FROM Course;')
+    await connection.close()
     return {
-        hello: 'from tbl! =)'
+        hello: 'from tbl! =)',
+        data: data.recordset
     }
 })
